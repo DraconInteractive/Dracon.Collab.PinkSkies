a7b3af9c12257eecc1eb58cd4dfb48ecc1dacee3
Peter Carey <rarceth@gmail.com>
2015-11-10 11:33:11 +0800

    Movement and Camera
    Created script for Camera and added movement controls to player

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+	public Vector3 desiredPos, currentPos;
+	public int camZOffset, camYOffset, camSyncRate;
+	public GameObject playerObj;
+	public bool isFree;
+	// Use this for initialization
+	void Start () {
+		playerObj = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void DetectInputs(){
+		if (Input.GetKey(KeyCode.Mouse0)){
+			isFree = true;
+		} else {
+			isFree = false;
+		}
+	}
+
+	public void CamMove (){
+		desiredPos = playerObj.transform.position - playerObj.transform.forward * camZOffset + playerObj.transform.up * camYOffset;
+		currentPos = Vector3.Lerp(currentPos, desiredPos, camSyncRate);
+		transform.position = currentPos;
+		Quaternion.LookRotation(currentPos - playerObj.transform.position, Vector3.up);
+	}
+}
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -2,15 +2,30 @@
 using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
-
+	public Rigidbody playerRigid;
+	public float speed;
 	// Use this for initialization
 	void Start () {
-		//This is the player script
-		//This is a fuck off sword
+		playerRigid = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		PlayerInput();
+	}
+
+	public void PlayerInput(){
+		//Set the players direction = to the horizontal and vertical input axis of the keyboard input (WASD and arrow keys)
+		Vector3 _Direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+		//Apply the direction to the character, with modifiers of speed and incrementation
+		playerRigid.MovePosition (transform.position + _Direction * speed * Time.deltaTime);
+		if (_Direction.magnitude > 0){
+			transform.forward = _Direction;
+		}
+		
+		//Keep a jumping function tucked away JIC
+		//if (Input.GetKeyDown(KeyCode.Space))
+		//    rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 	}
 }
