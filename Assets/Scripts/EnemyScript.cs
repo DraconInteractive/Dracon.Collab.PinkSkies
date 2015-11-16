496858d4df72bbb7464d43947003481aacc9a905
Scruby1 <scrubs_awesome@hotmail.com>
2015-11-16 16:09:34 +0800

    Enemy Initial setup
    Enemy edits

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,16 +4,37 @@ using System.Collections;
 public class EnemyScript : MonoBehaviour {
 	public int health, playerDamageRecieved;
 	public bool attackable;
-	// Use this for initialization
+
+	private GameObject player;
+	private float speed = 20, maxSpeed = 2, attackSpdTimer = 0, attackSpd = 1, triggerDisMax, triggerDisMin, meleeDis;
+	private Rigidbody rb;
+
 	void Start () {
 		health = 100;
 		attackable = false;
+		player = GameObject.FindWithTag ("Player");
+		rb = GetComponent<Rigidbody> ();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		AttackSequence();
 		HealthCheck();
+		EnemyMovement ();
+	}
+
+	void EnemyMovement(){
+
+		attackSpdTimer -= Time.deltaTime;
+
+		rb.velocity = Vector3.ClampMagnitude (rb.velocity, maxSpeed);
+
+		float distance = Vector3.Distance (transform.position, player.transform.position);
+
+		if (distance <= triggerDisMax && distance >= triggerDisMin) {//chasing
+			Vector3 playerPos = new Vector3 (player.transform.position.x, transform.position.y, player.transform.position.z);
+			transform.LookAt (playerPos);
+			GetComponent<Rigidbody> ().AddForce (transform.forward * speed);
+		} 
 	}
 
 	public void AttackSequence(){
