aa539ade0d70171e95ff7ce92eb6eb763ef6962c
Peter Carey <rarceth@gmail.com>
2015-11-30 14:56:55 +0800

    Major Test Scene Changes and GSScript Implementation

---
+++ Assets/Scripts/GSScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GSScript : MonoBehaviour {
+	public GameObject playerObj;
+	public Vector3 holsteredPos, attackPos;
+	// Use this for initialization
+	void Start () {
+		holsteredPos = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DetectAttackState();
+	}
+
+	public void DetectAttackState(){
+		if (playerObj.GetComponent<PlayerScript>().inCombat == true){
+
+		} else {
+
+		}
+	}
+}
