3e88ad52b141b27dee899cc572a6cc18d3d9f41c
EsquireGames <mark.thompson@hotmail.com.au>
2015-12-08 15:04:29 +0800

    AUdioInput

---
+++ Assets/Marks Level/AudioPLayScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioPLayScript : MonoBehaviour {
+
+	public AudioClip myAudio;
+	bool hasPlayed;
+
+
+	void Start () {
+
+	}
+	
+
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (!hasPlayed && other.tag == "Player") {
+			GetComponent <AudioSource> ().PlayOneShot (myAudio);
+			hasPlayed = true;
+		}
+	}
+}
